274a550eb83789067ba7f34cc78cdb658951ecde
songjimmy <519955504@qq.com>
2020-03-05 18:27:01 +0800

    a epoll version

--- Hasenohr/poller.cpp
+++ Hasenohr/poller.cpp
@@ -1,115 +1,126 @@
 #include "poller.h"
-#include "channel.h"
-#include <unistd.h>
-#include <stdio.h>
-#include <iostream>
-poller::poller(event_loop* owner_loop) :owner_loop_(owner_loop)
-{
-}
 
+poller::poller(event_loop* owner_loop__)
+{
+	owner_loop_ = owner_loop__;
+	epfd = ::epoll_create(1024);
+}
 
 muduo::Timestamp poller::poll(int time_out_ms, channel_list* active_channels)
 {
-	int active_num=::poll(pollfd_list_.data(), pollfd_list_.size(), time_out_ms);
-	LOG_INFO << "poll active."<< active_num;
+	assert_in_loop_thread();
+	active_events_.clear();
+	active_events_.resize(channel_map_.size());
+	int active_num = ::epoll_wait(epfd,active_events_.data(),int(active_events_.size()), time_out_ms);
 	muduo::Timestamp now_ = muduo::Timestamp::now();
 	if (active_num < 0)
 	{
-		LOG_ERROR << "\nwrong poll\n";
+		LOG_ERROR << "wrong poll";
+		LOG_ERROR << errno;
 	}
-	else if (active_num ==0)
+	else if (0==active_num)
 	{
-		LOG_TRACE << "\nnoting happend\n";
+		LOG_TRACE << "noting happend\n";
 	}
 	else
 	{
-		fill_active_channels(active_channels, active_num);
+		fill_active_channels(active_channels,active_num);
 	}
 	return now_;
 }
-
 
 void poller::assert_in_loop_thread()
 {
 	owner_loop_->assert_in_loop_thread();
 }
-//不在channel_map中的和在其中的
-
 
 void poller::update_channel(channel* channel_)
 {
-	channel_map_.size();
-	auto it = channel_map_.end();
-	if(!channel_map_.empty())
-		it = channel_map_.find(channel_->fd());
-	//在map中找到了
-	if ( it!= channel_map_.end())
+	int fd = channel_->fd();
+	//添加
+	if (channel_->index()==out|| channel_->index()==init)
 	{
-		channel* channel_in_map = it->second;
-		assert(channel_in_map->fd() == channel_->fd());
-		size_t index_ = channel_in_map->index();
-		assert(index_ <= pollfd_list_.size());
-		pollfd_list_[index_].events = short(channel_->events());
-		pollfd_list_[index_].revents = 0;
+		assert((channel_->index() == init&&channel_map_.find(fd) == channel_map_.end())|| (channel_->index() == out && channel_map_.find(fd) != channel_map_.end()));
+		insert_event(channel_);
+		channel_map_.insert({ fd,channel_ });
+		channel_->set_index(in);
+	}
+	//修改
+	else
+	{
+		assert(channel_map_.find(fd) != channel_map_.end());
+		assert(channel_map_[fd] == channel_);
 		if (channel_->is_none_event())
-			pollfd_list_[index_].fd = -pollfd_list_[index_].fd -1;
-		else pollfd_list_[index_].fd = channel_->fd();
-	}
-	else
-	{
-		pollfd new_pollfd;
-		new_pollfd.events = short(channel_->events());
-		new_pollfd.revents = 0;
-		new_pollfd.fd = channel_->fd();
-		pollfd_list_.push_back(new_pollfd);
-		channel_->set_index(int(pollfd_list_.size()) - 1);
-		channel_map_[channel_->fd()] = channel_;
-	}
-}
-
-void poller::fill_active_channels(channel_list* active_channels, int num_active)
-{
-	for (auto& pollfd_it:pollfd_list_)
-	{
-		if (num_active>0&&pollfd_it.revents>0)
 		{
-			--num_active;
-			auto it = channel_map_.find(pollfd_it.fd);
-			assert(it != channel_map_.end());
-			channel* active_channel = it->second;
-			assert(active_channel->fd() == pollfd_it.fd);
-			//更新了激活事件所属channel的实际事件，为下一步event_handle做准备
-			active_channel->set_revents(pollfd_it.revents);
-			active_channels->push_back(active_channel);
+			delete_event(channel_);
+			channel_->set_index(out);
+		}
+		else
+		{
+			modify_event(channel_);
 		}
 	}
 }
 
-void poller::remove_channel(channel* channel)
+void poller::remove_channel(channel* channel_)
 {
-	owner_loop_->assert_in_loop_thread();
-	assert(channel != nullptr);
-	assert(channel_map_.find(channel->fd()) != channel_map_.end());
-	int id_ = channel->index();
-	if (id_ == int(pollfd_list_.size()-1))
+	int fd = channel_->fd();
+	assert(channel_map_.find(fd) != channel_map_.end());
+	assert(channel_map_[fd] == channel_);
+	if (channel_->index() == in)
 	{
-		pollfd_list_.erase(pollfd_list_.end()-1);
+		delete_event(channel_);
 	}
 	else
 	{
-		int final_fd_ = (pollfd_list_.end() - 1)->fd;
-		if (final_fd_ < 0)
-		{
-			final_fd_ = -final_fd_ - 1;
-		}
-		std::iter_swap(pollfd_list_.end()-1,pollfd_list_.begin()+id_);
-		//assert(channel_map_.find(final_fd_) != channel_map_.end());
-		channel_map_[final_fd_]->set_index(id_);
-		pollfd_list_.erase(pollfd_list_.end() - 1);
+		assert(channel_->index() == out);
 	}
-	channel_map_.erase(channel->fd());
+	channel_map_.erase(channel_map_.find(fd));
 }
 
+void poller::fill_active_channels(channel_list* active_channels,int active_num)
+{
+	for (int i=0;i<active_num;++i)
+	{
+		epoll_event& event_ = active_events_[i];
+		channel* active_channel_ = (channel*)event_.data.ptr;
+		active_channel_->set_revents(event_.events);
+		active_channels->push_back(active_channel_);
+	}
+}
 
+void poller::insert_event(channel* channel_)
+{
+	epoll_event epoll_event_;
+	bzero(&epoll_event_, sizeof epoll_event_);
+	epoll_event_.data.ptr = channel_;
+	epoll_event_.events = channel_->events();
+	if (::epoll_ctl(epfd, EPOLL_CTL_ADD, channel_->fd(), &epoll_event_) < 0)
+	{
+		LOG_ERROR << "poller::insert_event at fd="<<channel_->fd();
+	}
+}
 
+void poller::modify_event(channel* channel_)
+{
+	epoll_event epoll_event_;
+	bzero(&epoll_event_, sizeof epoll_event_);
+	epoll_event_.data.ptr = channel_;
+	epoll_event_.events = channel_->events();
+	if (::epoll_ctl(epfd, EPOLL_CTL_MOD, channel_->fd(), &epoll_event_) < 0)
+	{
+		LOG_ERROR << "poller::modify_event at fd=" << channel_->fd();
+	}
+}
 
+void poller::delete_event(channel* channel_)
+{
+	epoll_event epoll_event_;
+	bzero(&epoll_event_, sizeof epoll_event_);
+	epoll_event_.data.ptr = channel_;
+	epoll_event_.events = channel_->events();
+	if (::epoll_ctl(epfd, EPOLL_CTL_DEL, channel_->fd(), &epoll_event_))
+	{
+		LOG_ERROR << "poller::delete_event at fd=" << channel_->fd();
+	}
+}
--- Hasenohr/poller.h
+++ Hasenohr/poller.h
@@ -2,20 +2,19 @@
 #include <vector>
 #include <map>
 #include <poll.h>
+#include <sys/epoll.h>
 #include <muduo/base/Timestamp.h>
 #include <muduo/base/Logging.h>
 #include "event_loop.h"
 
 class channel;
-struct pollfd;
 class poller
 {
 public:
-	typedef int fd_t;
 	typedef std::vector<channel*> channel_list;
 	//构造函数，传入一个channel_list用来转化为channel_map，也能同时获得event和revent__可能不需要 因为初始化时为空
 	//同时传入event_loop* 将poller和其所属loop绑定
-	poller(event_loop* owner_loop);
+	poller(event_loop* owner_loop__);
 	//不可拷贝
 	poller(const poller&) = delete;
 	//缺省析构函数
@@ -25,16 +24,27 @@ public:
 	void assert_in_loop_thread();
 	//更新？？？
 	void update_channel(channel* channel_);
-	void remove_channel(channel* channel);
+	void remove_channel(channel* channel_);
 private:
-	//根据活动的channels数量查询活动channel,并通知channel
-	void fill_active_channels(channel_list* active_channels,int num_active);
-	typedef std::vector<pollfd> pollfd_list;
-	typedef std::map<fd_t, channel*> channel_map;
-	//pollfd的数组，用来维护poll函数所需要的pollfd数组
-	pollfd_list pollfd_list_;
+	enum index_state { init = -1,out,in };
+	typedef std::map<int, channel*> channel_map;
+	typedef std::vector<epoll_event> event_list;
+	
+	//根据活动的channels数量查询活动channel,并通知channel更新其实际事件状态
+	void fill_active_channels(channel_list* active_channels,int active_num);
+
+	//以下操作只对epfd进行操作
+	void insert_event(channel* channel_);
+	void modify_event(channel* channel_);
+	void delete_event(channel* channel_);
+	
+	//eoll的特殊文件符
+	int epfd;
 	//用来查询特定fd_t对应的channel,从而通知channel进行相关的操作
 	channel_map channel_map_;
+	//
+	event_list event_list_;
+	event_list active_events_;
 	//所属的event_loop的指针
 	event_loop* owner_loop_;
 };
